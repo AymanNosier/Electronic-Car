ef6850c6eadddf06d1779e26f13313c79eb7531f
Ayman Nosier <119597392+AymanNosier@users.noreply.github.com>
2022-12-04 13:53:55 -0800

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,111 @@
+let name = prompt(" please inter your name")
+alert("happy " + "day " + "ENG " + name)
+
+function f1() {
+
+    document.getElementById("demo").style = "transform:translate(-2000px)"
+    document.getElementById("demo0").style = "transform:translate()"
+    document.getElementById("a").style = "transform:rotate(-360deg)"
+    document.getElementById("b").style = "transform:rotate(-360deg)"
+    document.getElementById("h").style.backgroundColor = "orange";
+    document.getElementById("s").style.backgroundColor = "red";
+
+
+
+
+}
+function f2() {
+
+    document.getElementById("demo").style = "transform:translate(-4500px)"
+    document.getElementById("demo0").style = "transform:translate(-3200px)"
+    document.getElementById("a").style = "transform:rotate(600deg)"
+    document.getElementById("b").style = "transform:rotate(600deg)"
+    document.getElementById("h").style.backgroundColor = "red";
+    document.getElementById("s").style.backgroundColor = "orange";
+    document.getElementById("a5").style.backgroundColor = "yellow";
+
+}
+function f3() {
+
+    document.getElementById("demo").style = "transform:translate(-4000PX)"
+    document.getElementById("demo0").style = "transform:translate(-3000px)"
+    document.getElementById("a").style = "transform:rotate(360deg)"
+    document.getElementById("b").style = "transform:rotate(360deg)"
+    document.getElementById("h").style.backgroundColor = "orange";
+}
+function a() {
+
+
+    document.getElementById("a5").style.backgroundColor = "white";
+
+
+}
+function e() {
+    document.getElementById("h").style.backgroundColor = "red";
+    document.getElementById("s").style.backgroundColor = "red";
+}
+function f() {
+    document.getElementById("h").style.backgroundColor = "orange";
+    document.getElementById("s").style.backgroundColor = "orange";
+}
+
+function b() {
+
+    document.getElementById("a5").style.backgroundColor = "yellow";
+
+}
+function c() {
+
+    document.getElementById("aa").style = "transform: translateY(40px)";
+
+}
+function cc() {
+
+    document.getElementById("aa").style = "transform: translateY(0px)";
+}
+function d() {
+
+    document.getElementById("bb").style = "transform: translateY(40px)";
+
+}
+function dd() {
+
+    document.getElementById("bb").style = "transform: translateY(0px)";
+}
+function rc() {
+
+    document.getElementById("i").style = "transform: translateY(-40px)";
+    document.getElementById("j").style = "transform: translateY(-40px)";
+
+}
+function rv() {
+
+    document.getElementById("i").style = "transform: translateY(0px)";
+    document.getElementById("j").style = "transform: translateY(0px)";
+}
+
+
+function w1() {
+
+    document.getElementById("w").style = "transform: translateY(-100px)";
+    document.getElementById("ww").style = "transform: translateY(-100px)";
+    document.getElementById("aa").style = "background:rgb(255, 238, 0);border: none;";
+}
+function w2() {
+
+    document.getElementById("w").style = "transform: translateY(0px)";
+    document.getElementById("ww").style = "transform: translateY(0px)";
+    document.getElementById("aa").style = "transform: translateY(0px);border:none";
+}
+function z1() {
+
+    document.getElementById("z").style = "transform: translateY(-100px)";
+    document.getElementById("zz").style = "transform: translateY(-100px)";
+    document.getElementById("bb").style = "background:rgb(255, 238, 0);border: none;";
+}
+function z2() {
+
+    document.getElementById("z").style = "transform: translateY(0px)";
+    document.getElementById("zz").style = "transform: translateY(0px)";
+    document.getElementById("bb").style = "transform: translateY(0px)";
+}
